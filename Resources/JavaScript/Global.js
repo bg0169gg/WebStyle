49a9de7cc885706508d3ca33ce2dc4a3e91a6438
Boris Garmev <borisgarmev912@proton.me>
2024-06-15 16:01:52 +0300

    Major V2
    Lotta stuff

--- Resources/JavaScript/Global.js
+++ Resources/JavaScript/Global.js
@@ -1,23 +1,49 @@
 
-/* Default values for HTML elements and their CSS classes & IDs */
-const mouseleftalert = "The mouse has been moved out of the range of the headbar. TODO: Implement close func on mouse leave"
-var headbar_toggle = document.querySelector('.header-button')   /* The main headbar toggle button */
-var headbar_element = document.querySelector('headbar')         /* The headbar itself */
+// Logic for scroll-down button on homepage
 
-function ToggleHeadbar() {
-    var headbar_opened = '0'                                        /* Headbar status - 0:closed, 1:opened */
-    var headbar_opened_class = 'headbar-open'                       /* The assigned class on open bar */
-    var header_opened_class = 'header_headbar-open'                 /* The assigned header class on open bar */
-    var header_element = document.querySelector('header')           /* The header itself */
-    var headbar_element = document.querySelector('headbar')
+function ScrollBtn_Categories() {
+    window.scroll({
+        top: 700,
+        behavior: "smooth",
+    });
+}
 
-    if (headbar_opened = 0) {
-        headbar_element.classList.toggle(headbar_opened_class)
-        header_element.classList.toggle(header_opened_class)
-        var headbar_opened = '1'
-    } else if (headbar_opened = 1) {
-        headbar_element.classList.toggle(headbar_opened_class)
-        header_element.classList.toggle(header_opened_class)
-        var headbar_opened = '0'
+// Sidebar Open and close function
+
+function SidebarToggle() {
+    var Sidebar_element = document.querySelector('sidebar');
+    var Sidebar_Active = false;
+    var Sidebar_Active_class = 'page-sidebar_Active';
+    if (Sidebar_Active === false) {
+        Sidebar_element.classList.toggle(Sidebar_Active_class);
+        var Sidebar_Active = true;
+    } else if (Sidebar_Active === true) {
+        Sidebar_element.classList.toggle(Sidebar_Active_class);
+        var Sidebar_Active = false;
     }
-};
+}
+
+// Search Engine Logic and functions
+
+function SearchEngineCmd() {
+    const SearchQuery = document.getElementById("MainSearchEngineBox").value.toLowerCase();
+    if (SearchQuery) {
+        console.log("Starting search for:", SearchQuery);
+    } else {
+        console.log("Empty Search Bar!");
+    }
+}
+var HomepageSearchbox = document.querySelector("HomepageSearchbox");
+const SearchEngineObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            HomepageSearchbox.classList.add("HeaderSearchBoxActive");
+            console.log('Homepage Search out of viewport');
+        } else {
+            HomepageSearchbox.classList.remove("HeaderSearchBoxActive");
+            console.log('Homepage Search in viewport');
+        }
+    });
+});
+SearchEngineObserver.observe(HomepageSearchbox);
+
